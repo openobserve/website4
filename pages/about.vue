71b6e3da161cc95781d104a52d7a910684605714
krupa <krupa.jivani@kiara.tech>
2023-04-24 14:52:58 +0530

    add content in about json file and display in about page

--- pages/about.vue
+++ pages/about.vue
@@ -1,95 +1,29 @@
 <template>
-  <HeroSection :data="heroSection"></HeroSection>
-  <WhatWeDo :data="content" />
+  <HeroSection :data="data.heroSection"></HeroSection>
+  <WhatWeDo :data="data.aboutus" />
   <Section>
     <div 
-    :style="{ backgroundImage: `url(${items.backgroundImage})` }"
+    :style="{ backgroundImage: `url(${data.data.backgroundImage})` }"
       class="background py-16"
   >
-    <SectionHeading :title="data.heading" />
-    <Feature variant="1:1" :data="data" />
+    <Feature  :items="data.data.items" :title="data.data.heading" />
   </div>
 
   </Section>
   
-    <Investors :data="items" />
+    <Investors :data="data.items" />
 
 </template>
 <script setup>
-const seoData = {
-  title: "About Us",
-  desc: "",
-};
-const data = {
-  heading: "Why We Do",
-  title: "ZincObserve is Built for Full - Text Search",
-  desc: "ZincObserve is a search engine that can be used for text data, logs, metrics, events, and more. It allows you to do a full-text search, among other things, including sending server logs to ZincObserve, pushing your application data, providing a full-text search, or building a search bar in your application.",
-  image: "/img/feature.svg",
-};
-const heroSection = {
-  title: "Making Data-Driven Decisions Easier than Ever!",
-  subtitle: "",
-  image: "/img/aboutus/abouthero.png",
-  backgroundImage: "/img/aboutus/aboutusbg.png",
-};
-const content = {
-  title: "What We Do",
-  description:
-    "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce ligula metus, molestie rutrum rhoncus sit amet, maximus sit amet augue. Donec non magna enim. Fusce maximus lobortis auctor. Duis pretium ipsum sed blandit sollicitudin. Curabitur venenatis sapien non laoreet ornare. Aliquam tincidunt pharetra commodo.",
-};
-const items = {
-  title: "Investors",
-  backgroundImage:"/img/homepage/cardbg2.svg",
-  images1:[
-    "/img/aboutus/Investor1.png",
-    "/img/aboutus/Investor1.png",
-    "/img/aboutus/Investor1.png",
-  ],
-  images2: ["/img/aboutus/Investor1.png", "/img/aboutus/Investor1.png"],
-};
-// const cardItems = {
-//   title: "ZincObserve Features",
-//   subtitle: "It provides a host of features that help you get up and running in minutes.",
-//   items: [
-//     {
-//       image: "/img/CardIcon.svg",
-//       title: "Full Text Search",
-//       desc: "You can index large amounts of text Zinc and search instantly. Zinc is a batteries-included setup. Embedded UI provides an easy way to get started and interact with your data.",
-//       btnText: "",
-//     },
-//     {
-//       image: "/img/CardIcon.svg",
-//       title: "Full Text Search",
-//       desc: "Zinc is a batteries-included setup. Embedded UI provides an easy way to get started and interact with your data. No need to set up a separate GUI like Kibana.",
-//       btnText: "",
-//     },
-//     {
-//       image: "/img/CardIcon.svg",
-//       title: "Full Text Search",
-//       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-//       btnText: "",
-//     },
-//     {
-//       image: "/img/CardIcon.svg",
-//       title: "Full Text Search",
-//       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-//       btnText: "",
-//     },{
-//       image: "/img/CardIcon.svg",
-//       title: "Full Text Search",
-//       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-//       btnText: "",
-//     },
-//     {
-//       image: "/img/CardIcon.svg",
-//       title: "Full Text Search",
-//       desc: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
-//       btnText: "",
-//     }
-//   ]
-// }
+const { data } = await useAsyncData(async () => {
+  return await queryContent("/about").findOne();
+});
+console.log(data,"data");
+useSeoMeta({
+  title: data.title,
+  description: data.desc,
+});
 
-useSeoMeta(seoData);
 </script>
 <style scoped>
 .background {
