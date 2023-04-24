b57a86922bbca28b4ed8287038e7ab490d1f30c6
krupa <krupa.jivani@kiara.tech>
2023-04-24 15:11:13 +0530

    modified product page content

--- pages/products/zincobserve.vue
+++ pages/products/zincobserve.vue
@@ -1,21 +1,18 @@
 <template>
   <!-- <BreadCrumbs class="md:pt-0 pt-3" :title="data.title" :paths="data.breadcrumbs" /> -->
   <HeroSection :data="data.heroSection" />
-  <Feature :data="data.details" />
-  <SlideIn>
-    <Section>
-      <SectionHeading :title="data.content.title" />
-      <Feature v-for="(item, index) in data.content.items" :key="index" :direction="index % 2 == 0 ? 'left' : 'right'"
-        :data="item" />
-    </Section>
-  </SlideIn>
-  <!-- <Card :items="data.content" :backgroundImage="true" /> -->
-  <Card :items="data.features" :backgroundImage="true" />
+  <FeatureItem :data="data.details" />
+  <Feature :items="data.content.items" :title="data.content.title" />
+  <Cards :items="data.features" :backgroundImage="true" />
   <FaqSection :items="data.faqData" />
   <LatestBlogPosts />
 </template>
 <script setup>
 const { data } = await useAsyncData(async () => {
   return await queryContent("/products/zincobserve").findOne()
+})
+useSeoMeta({
+  title:data.title,
+  description:data.desc
 })
 </script>
--- pages/products/zplane.vue
+++ pages/products/zplane.vue
@@ -1,21 +1,16 @@
 <template>
   <!-- <BreadCrumbs class="md:pt-0 pt-3" :title="data.title" :paths="data.breadcrumbs" /> -->
   <HeroSection :data="data.heroSection" />
-
-  <!-- <Section>
-    <SectionHeading :title="data.items.heading" :subtitle="data.items.subheading" />
-    <Feature variant="1:1" direction="left" :data="data.items" />
-    <Feature variant="1:1" direction="right" :data="data.items" />
-    <Feature variant="1:1" direction="left" :data="data.items" />
-  </Section> -->
-  <Card :items="data.content"/>
-  <Section>
-    <FaqSection :items="data.faqData"/>
-  </Section>
+  <Cards :items="data.content"/>
+  <FaqSection :items="data.faqData"/>
   <LatestBlogPosts />
 </template>
 <script setup>
 const { data } = await useAsyncData(async () => {
   return await queryContent("/products/zplane").findOne()
+})
+useSeoMeta({
+  title:data.title,
+  description:data.desc
 })
 </script>
