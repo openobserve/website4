8c0e9aa9d2b21202242002df56eddea66f6e314f
Kirtan Thakkar <kirtan@kiara.tech>
2023-04-25 18:45:37 +0530

    update pricing title

--- components/pricing/pricing.vue
+++ components/pricing/pricing.vue
@@ -1,11 +1,10 @@
 <template>
   <Section>
-    <SectionHeading :title="items.title" />
-    <div class="container mx-auto px-4">
+    <SectionHeading :title="items.title" :subtitle="items.subtitle" />
+    <div class="container mx-auto px-4 mt-4">
       <div
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-x-4 gap-y-4"
       >
-        
         <Pricingitem :item="item" :key="index" v-for="(item, index) in items.priceItems"/>
       </div>
     </div>
