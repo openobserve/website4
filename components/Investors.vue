da6dca2b5f1eee34da30b5c9f266b6dbc1b5a459
krupa <krupa.jivani@kiara.tech>
2023-04-26 17:34:25 +0530

    modified investor section

--- components/Investors.vue
+++ components/Investors.vue
@@ -1,10 +1,20 @@
 <template>
- <Section>
-    <div class="w-full  py-10">
+  <Section>
+    <div class="w-full py-10">
       <SectionHeading :title="data.title" />
-      <div class="flex flex-col md:flex-row container mx-auto justify-center items-center">
-        <div class="" v-for="(item, index) in data.images" :key="index">
-          <img :src="item" alt="Investors" class="px-0 md:px-10 " />
+      <div class="container mx-auto">
+        <div class="flex flex-col md:flex-row justify-center space-x-8">
+          <div
+            class="shadow-xl bg-theme-primary-100 bg-opacity-50 h-full rounded-lg"
+            v-for="(item, index) in data.images"
+            :key="index"
+          >
+            <img
+              :src="item"
+              alt="Investors"
+              class="p-4 flex items-center w-72 h-72 object-contain justify-center"
+            />
+          </div>
         </div>
       </div>
     </div>
@@ -12,8 +22,8 @@
 </template>
 <script setup>
 defineProps({
-   data:{
-    type:Object
-   }
-})
+  data: {
+    type: Object,
+  },
+});
 </script>
