4ccf1fcbcd96950df9c90e8211c7f1a3d154b953
Kirtan Thakkar <kirtan@kiara.tech>
2023-04-27 17:16:54 +0530

    add star to the mobile view

--- components/header/MobileView.vue
+++ components/header/MobileView.vue
@@ -9,6 +9,10 @@
                         </div>
                     </nuxt-link>
                 </div>
+                <div class="flex-1"></div>
+                <div class="mr-2">
+                    <GithubCount />
+                </div>
                 <div class="" @click="isOpen = !isOpen">
                     <svg v-if="!isOpen" xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24"
                         stroke="currentColor">
