092dcd13bc62402a3b312b45b3f65fa338dde8e6
krupa <krupa.jivani@kiara.tech>
2023-05-02 12:24:00 +0530

    remove plus icon and expand collapsed fadqitems

--- components/common/FAQ/FaqItem.vue
+++ components/common/FAQ/FaqItem.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="container mx-auto shadow rounded-lg bg-white border border-gray-100 text-black" >
-        <div class="flex justify-between items-center cursor-pointer px-5 py-4" :class="clicked" @click="handleclick">
+        <div class="flex justify-between items-center cursor-pointer px-5 py-4">
             <div class=" text-sm md:text-xl font-semibold ">{{ item.question }}</div>
-            <div v-if="clicked"><img src="/img/icons/Minus.svg" alt="minus icon" /></div>
-            <span v-else> <img src="/img/icons/Plus.svg" alt="plusicon" /> </span>
+            <!-- <div v-if="clicked"><img src="/img/icons/Minus.svg" alt="minus icon" /></div>
+            <span v-else> <img src="/img/icons/Plus.svg" alt="plusicon" /> </span> -->
         </div>
-        <div class="container mx-auto px-5 text-black" :class="clicked ? 'hidden':''">
+        <div class="container mx-auto px-5 text-black">
             <div className="text-sm py-2 leading-2 tracking-wider" >{{ item.answer }}</div>
         </div>
     </div>
