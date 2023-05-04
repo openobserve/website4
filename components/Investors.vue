6c0d44025660d53f026bc4ec3dad502e134c2af6
krupa <krupa.jivani@kiara.tech>
2023-05-04 15:44:56 +0530

    fix mobile responsive investor section

--- components/Investors.vue
+++ components/Investors.vue
@@ -2,8 +2,8 @@
   <Section>
     <div class="w-full py-10">
       <SectionHeading :title="data.title" />
-      <div class="container mx-auto">
-        <div class="flex flex-col md:flex-row justify-center space-x-8">
+      <div class="container mx-auto px-4 md:px-0">
+        <div class="flex flex-col md:flex-row justify-center space-x-0 md:space-x-8 space-y-4 md:space-y-0">
           <div
             class="shadow-xl bg-theme-primary-100 bg-opacity-50 h-full rounded-lg"
             v-for="(item, index) in data.images"
@@ -12,7 +12,7 @@
             <img
               :src="item"
               alt="Investors"
-              class="p-4 flex items-center w-72 h-72 object-contain justify-center"
+              class="p-4 block mx-auto object-contain object-center "
             />
           </div>
         </div>
