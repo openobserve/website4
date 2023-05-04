79d206f64622fc0ffbba743c5640fe22237223be
krupa <krupa.jivani@kiara.tech>
2023-05-04 13:52:49 +0530

    update feature item compoenent

--- components/common/Feature/FeatureItem.vue
+++ components/common/Feature/FeatureItem.vue
@@ -1,66 +1,66 @@
 <template>
-    <Section>
-      <div class="px-4 container mx-auto">
-        <div :class="[directions[direction]]">
+  <Section>
+    <div class="px-4 container mx-auto">
+      <div :class="[directions[direction]]">
+        <div
+          :class="[variation[variant].el1]"
+          class="flex items-center bg-no-repeat bg-auto object-contain px-0 md:px-16"
+        >
+          <img
+            :src="data.image"
+            class="object-contain"
+            :alt="data.title || data.tagline"
+          />
+        </div>
+        <div
+          :class="[
+            'md:pt-0 pt-5',
+            direction == 'left' ? 'md:pl-8' : 'md:pr-8',
+            variation[variant].el2,
+          ]"
+        >
           <div
-            :class="[variation[variant].el1]"
-            class="flex items-center bg-no-repeat bg-auto object-contain px-0 md:px-16"
+            v-if="data.tagline"
+            class="text-center md:text-left text-xs md:text-sm lg:text-sm mb-4"
           >
-            <img
-              :src="data.image"
-              class="object-contain"
-              :alt="data.title || data.tagline"
-            />
+            <span
+              class="bg-theme-primary-100 rounded-full text-theme-primary-600 px-4 py-1 tracking-widest capitalize"
+            >
+              {{ data.tagline }}</span
+            >
           </div>
+          <div>
+            <h2
+              class="text-base md:text-3xl text-center md:text-left font-bold text-black inline-block"
+              v-html="data.title || ''"
+            ></h2>
+          </div>
+          <div>
+            <h3
+              class="text-sm md:text-base text-center md:text-left italic mb-4 text-black inline-block"
+              v-html="data.subtitle || ''"
+            ></h3>
+          </div>
+          <p
+            class="text-justify text-sm leading-relaxed font-normal mt-1 md:mt-3"
+            v-html="data.desc"
+          ></p>
           <div
-            :class="[
-              'md:pt-0 pt-5',
-              direction == 'left' ? 'md:pl-8' : 'md:pr-8',
-              variation[variant].el2,
-            ]"
+            v-if="data.btnText"
+            class="flex justify-center md:justify-start py-5"
           >
-            <div
-              v-if="data.tagline"
-              class="text-center md:text-left text-xs md:text-sm lg:text-sm mb-4"
+            <Button
+              :variant="data.btnVariant ? data.btnVariant : 'primary'"
+              :target="data.btnTarget"
+              :to="data.btnTo"
             >
-              <span
-                class="bg-theme-primary-100 rounded-full text-theme-primary-600 px-4 py-1 tracking-widest capitalize"
-              >
-                {{ data.tagline }}</span
-              >
-            </div>
-            <div>
-              <h2
-                class="text-base md:text-3xl text-center md:text-left font-bold text-black inline-block"
-                v-html="data.title || ''"
-              ></h2>
-            </div>
-            <div>
-              <h3
-                class="text-sm md:text-base text-center md:text-left italic mb-4 text-black inline-block"
-                v-html="data.subtitle || ''"
-              ></h3>
-            </div>
-            <p
-              class="text-justify text-sm leading-relaxed font-normal mt-1 md:mt-3"
-              v-html="data.desc"
-            ></p>
-            <div
-              v-if="data.btnText"
-              class="flex justify-center md:justify-start py-5"
-            >
-              <Button
-                :variant="data.btnVariant ? data.btnVariant : 'primary'"
-                :target="data.btnTarget"
-                :to="data.btnTo"
-              >
-                {{ data.btnText }}
-              </Button>
-            </div>
+              {{ data.btnText }}
+            </Button>
           </div>
         </div>
       </div>
-    </Section>
+    </div>
+  </Section>
 </template>
 <script setup>
 const variation = {
