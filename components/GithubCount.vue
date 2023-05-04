9c7933f2c4f006656aa4d3497755c76efa8500e6
krupa <krupa.jivani@kiara.tech>
2023-05-04 16:13:26 +0530

    apply color theme github button

--- components/GithubCount.vue
+++ components/GithubCount.vue
@@ -1,6 +1,6 @@
 <template>
  <!-- Place this tag where you want the button to render. -->
-<github-button href="https://github.com/zinclabs/zincobserve" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Star zinclabs/zincobserve on GitHub">Star</github-button>
+<github-button href="https://github.com/zinclabs/zincobserve"  data-color-scheme="no-preference: light; light: light; dark: light;" data-size="large" data-show-count="true" aria-label="Star zinclabs/zincobserve on GitHub">Star</github-button>
 </template>
 <script setup>
 import GithubButton from 'vue-github-button'
--- components/GithubForkCount.vue
+++ components/GithubForkCount.vue
@@ -1,6 +1,6 @@
 <template>
 <!-- Place this tag where you want the button to render. -->
-<github-button href="https://github.com/zinclabs/zincobserve/fork" data-color-scheme="no-preference: light; light: light; dark: dark;" data-size="large" data-show-count="true" aria-label="Fork zinclabs/zincobserve on GitHub">Fork</github-button>
+<github-button href="https://github.com/zinclabs/zincobserve/fork" data-color-scheme="no-preference: light; light: light; dark: light;" data-size="large" data-show-count="true" aria-label="Fork zinclabs/zincobserve on GitHub">Fork</github-button>
 </template>
 <script setup>
 import GithubButton from 'vue-github-button'
