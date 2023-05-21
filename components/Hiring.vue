58aaa6ee0ebe0f751e76a2fa35071b359cf66a71
Kirtan Thakkar <kirtan@kiara.tech>
2023-05-21 20:40:11 +0530

    add basic job description

--- components/Hiring.vue
+++ components/Hiring.vue
@@ -1,18 +1,25 @@
 <template>
-    <Section>
-        <SectionHeading :title='data.title' :subtitle="data.subtitle"/>
+    <Section v-if="data.jobs">
+        <SectionHeading :title='data.title' :subtitle="data.subtitle" />
         <div class="container mx-auto px-4 ">
-            <div class="flex flex-col md:flex-row justify-between md:justify-around items-center py-3" v-for="(item,index) in data.jobs">
-                <p class="font-bold text-lg md:text-xl text-black mb-4 md:mb-0">&#9673; {{ item.title }} </p>
-                <Button variant="primary" :to="item.btnLink" target="_blank">{{ item.btnText  }}</Button>
+            <div v-for="(item, index) in data.jobs">
+                <div class="flex flex-row md:flex-row justify-items-center justify-between py-3">
+                    <div class="font-bold text-lg md:text-xl py-2 text-black">&#9673; {{ item.title }} </div>
+                    <Button variant="primary" :to="item.btnLink" target="_blank">{{ item.btnText }}</Button>
+                </div>
+                <div v-if="item.description">
+                    <div class="text-gray-600 mb-2">Job Description</div>
+                    <div v-html="item.description"></div>
+                </div>
+                <hr class="border my-4">
             </div>
         </div>
     </Section>
 </template>
 <script setup>
 defineProps({
-    data:{
-        type:Object
+    data: {
+        type: Object
     }
 })
 </script>
