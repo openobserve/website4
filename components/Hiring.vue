43291156165a58c244fa073b5fd205176c130e1a
krupa <krupa.jivani@kiara.tech>
2023-05-22 17:21:29 +0530

    working on file type pdf

--- components/Hiring.vue
+++ components/Hiring.vue
@@ -1,8 +1,8 @@
 <template>
-    <Section v-if="data.jobs">
-        <SectionHeading :title='data.title' :subtitle="data.subtitle" />
-        <div class="container mx-auto px-4 ">
-            <div v-for="(item, index) in data.jobs">
+  <Section v-if="data.jobs">
+    <SectionHeading :title="data.title" :subtitle="data.subtitle" />
+    <div class="container mx-auto px-4 py-4 flex flex-col md:flex-row space-x-6 ">
+      <!-- <div v-for="(item, index) in data.jobs">
                 <div class="flex flex-row md:flex-row justify-items-center justify-between py-3">
                     <div class="font-bold text-lg md:text-xl py-2 text-black">&#9673; {{ item.title }} </div>
                     <Button variant="primary" :to="`/career/apply?job=${item.id}`">{{ item.btnText }}</Button>
@@ -12,14 +12,31 @@
                     <div v-html="item.description"></div>
                 </div>
                 <hr class="border my-4">
-            </div>
+            </div> -->
+      <div
+        class="bg-white border border-theme-primary-300 p-4 shadow rounded-lg"
+        v-for="(item, index) in data.jobs"
+      >
+        <div class="">
+          <div class="font-bold text-lg md:text-xl py-2 text-black">
+            &#9673; {{ item.title }}
+          </div>
+          <div v-if="item.description">
+            <div class="text-gray-600 mb-2">Job Description</div>
+            <div v-html="item.description" class="text-sm"></div>
+          </div>
+          <Button variant="primary" :to="`/career/apply?job=${item.id}`">{{
+            item.btnText
+          }}</Button>
         </div>
-    </Section>
+      </div>
+    </div>
+  </Section>
 </template>
 <script setup>
 defineProps({
-    data: {
-        type: Object
-    }
-})
+  data: {
+    type: Object,
+  },
+});
 </script>
