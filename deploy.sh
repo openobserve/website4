6481942ade7cb7af6a64ba6331e83d7e4a6f9f42
Prabhat Sharma <hi.prabhat@gmail.com>
2023-05-31 09:34:39 +0530

    cloudfront invalidation updated

--- deploy.sh
+++ deploy.sh
@@ -6,4 +6,4 @@ npm run generate
 aws s3 sync ./.output/public s3://zincsearch-website/  --exclude=".git/*" --profile=zinc-prod
 
 # invalidate cloudfront cache so that latest files can be served
-aws cloudfront create-invalidation --distribution-id E36UKF51QZRZ7C --paths="/*" --profile=zinc-prod
+aws cloudfront create-invalidation --distribution-id E3KB2JN7JA8U38 --paths="/*" --profile=zinc-prod
