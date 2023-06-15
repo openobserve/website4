e537c73dca22652472de69502fba49255f7fc409
krupa <krupa.jivani@kiara.tech>
2023-06-15 11:17:37 +0530

    update css

--- components/Hiring.vue
+++ components/Hiring.vue
@@ -3,10 +3,10 @@
     <SectionHeading :title="data.title" :subtitle="data.subtitle" />
     <div class="container mx-auto flex flex-wrap justify-center px-4 md:px-0">
       <div
-        class="flex flex-col bg-white border border-theme-primary-300 p-4 shadow rounded-lg w-full md:w-[calc(33.33333%-20px)] m-[10px]"
+        class="flex flex-col bg-white border border-theme-primary-300 p-4 shadow rounded-lg w-full md:w-[calc(50%-20px)] m-[10px]"
         v-for="(item, index) in data.jobs"
       >
-        <div class="font-bold text-lg md:text-xl py-2 text-black">
+        <div class="font-bold text-lg md:text-xl py-2 text-black" :key="index">
           &#9673; {{ item.title }}
         </div>
         <div v-if="item.description" class="flex-1 mb-3">
