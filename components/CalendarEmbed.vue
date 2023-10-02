1497ac6d03c8cba0964ebd28ed159b39ccf78ee6
Kirtan Thakkar <kirtan@kiara.tech>
2023-10-02 14:57:54 +0530

    update contact us

---
+++ components/CalendarEmbed.vue
@@ -0,0 +1,61 @@
+<template>
+  <div
+    style="width: 100%; height: 100%; overflow: scroll"
+    id="my-cal-inline"
+  ></div>
+</template>
+
+<script>
+export default {
+  mounted() {
+    this.embedCalendar();
+  },
+  methods: {
+    embedCalendar() {
+      (function (C, A, L) {
+        let p = function (a, ar) {
+          a.q.push(ar);
+        };
+        let d = C.document;
+        C.Cal =
+          C.Cal ||
+          function () {
+            let cal = C.Cal;
+            let ar = arguments;
+            if (!cal.loaded) {
+              cal.ns = {};
+              cal.q = cal.q || [];
+              d.head.appendChild(d.createElement("script")).src = A;
+              cal.loaded = true;
+            }
+            if (ar[0] === L) {
+              const api = function () {
+                p(api, arguments);
+              };
+              const namespace = ar[1];
+              api.q = api.q || [];
+              typeof namespace === "string"
+                ? (cal.ns[namespace] = api) && p(api, ar)
+                : p(cal, ar);
+              return;
+            }
+            p(cal, ar);
+          };
+      })(window, "https://app.cal.com/embed/embed.js", "init");
+      Cal("init", { origin: "https://cal.com" });
+
+      Cal("inline", {
+        elementOrSelector: "#my-cal-inline",
+        calLink: "prabhatsharma/30min",
+        layout: "month_view",
+      });
+
+      Cal("ui", {
+        styles: { branding: { brandColor: "#ED4725" } },
+        hideEventTypeDetails: false,
+        layout: "month_view",
+      });
+    },
+  },
+};
+</script>
--- pages/contactus.vue
+++ pages/contactus.vue
@@ -14,60 +14,6 @@
         >
           <!-- START Contact Details-->
           <img :src="data.content.image" :alt="data.content.title" />
-          <div>
-            <div class="text-3xl">{{ data.content.title }}</div>
-            <div>
-              <div class="w-full flex flex-col items-center space-y-4 py-4">
-                <div class="w-full flex flex-col space-y-4">
-                  <div class="flex">
-                    <img
-                      :src="data.content.address.icon"
-                      class="max-h-12"
-                      :alt="data.content.address.title"
-                    />
-                    <div class="flex-1">
-                      <div class="font-semibold text-lg">
-                        {{ data.content.address.title }}
-                      </div>
-                      <div class="text-sm">
-                        <p>{{ data.content.address.text }}</p>
-                      </div>
-                    </div>
-                  </div>
-                  <div class="flex">
-                    <img
-                      :src="data.content.mail.icon"
-                      class="max-h-12"
-                      :alt="data.content.mail.title"
-                    />
-                    <div class="flex-1">
-                      <div class="font-semibold text-lg">
-                        {{ data.content.mail.title }}
-                      </div>
-                      <a :href="'mailto:' + data.content.mail.text">{{
-                        data.content.mail.text
-                      }}</a>
-                    </div>
-                  </div>
-                  <div class="flex">
-                    <img
-                      :src="data.content.phone.icon"
-                      class="max-h-12"
-                      :alt="data.content.phone.title"
-                    />
-                    <div class="flex-1">
-                      <div class="font-semibold text-lg">
-                        {{ data.content.phone.title }}
-                      </div>
-                      <a :href="'tel:' + data.content.phone.text">{{
-                        data.content.phone.text
-                      }}</a>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
         </div>
 
         <div
@@ -82,69 +28,7 @@
             class="leading-relaxed mb-5 text-theme-text-primary"
             v-html="data.content.subheading"
           ></p>
-          <form ref="form" @submit="submitdata">
-            <ZInput
-              ref="inputName"
-              id="name"
-              name="name"
-              required
-              label="Name"
-              v-model="formValue.name"
-            />
-            <ZInput
-              ref="inputEmail"
-              id="email"
-              name="email"
-              required
-              label="Email"
-              v-model="formValue.email"
-            />
-            <ZInput
-              ref="inputPhone"
-              id="phone"
-              name="phone"
-              label="Phone"
-              v-model="formValue.phone"
-            />
-            <ZInput
-              ref="inputWebsiteUrl"
-              id="websiteurl"
-              name="websiteurl"
-              label="Website"
-              type="url"
-              v-model="formValue.websitelink"
-            />
-            <TextArea
-              ref="inputMessage"
-              id="message"
-              name="message"
-              required
-              label="Message"
-              v-model="formValue.message"
-            ></TextArea>
-            <div
-              v-if="error"
-              class="px-4 py-2 bg-red-500 text-white border border-red-700 rounded-md"
-            >
-              {{ error }}
-            </div>
-            <div class="flex justify-center md:justify-start py-5 w-full">
-              <component
-                is="button"
-                type="submit"
-                variant="primary"
-                class="px-6 bg-theme-primary-500 rounded-md py-2 text-white"
-                :disabled="loading"
-                :class="
-                  loading ? 'bg-theme-primary-300' : 'bg-theme-primary-500'
-                "
-              >
-                {{
-                  loading ? data.content.btnLoadingText : data.content.btnText
-                }}
-              </component>
-            </div>
-          </form>
+          <CalendarEmbed />
         </div>
       </div>
     </div>
