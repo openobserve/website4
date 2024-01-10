4087a168ac3fbcdd102e3fe2ee83afba3a6d354e
Prabhat Sharma <hi.prabhat@gmail.com>
2024-01-09 18:42:42 -0800

    update

--- public/js/zinc.js
+++ public/js/zinc.js
@@ -1,4 +1,62 @@
 (function() {
+  // Function to extract the domain from a URL
+  function getDomainFromUrl(url) {
+    var a = document.createElement('a');
+    a.href = url;
+    return a.hostname; // Returns the domain part of the URL
+  }
+
+  // Function to get browser and OS information
+  function getBrowserAndOS() {
+    var userAgent = navigator.userAgent;
+    var browserName, osName;
+
+    // Detect browser name
+    if (userAgent.match(/chrome|chromium|crios/i)) {
+      browserName = 'Chrome';
+    } else if (userAgent.match(/firefox|fxios/i)) {
+      browserName = 'Firefox';
+    } else if (userAgent.match(/safari/i)) {
+      browserName = 'Safari';
+    } else if (userAgent.match(/opr\//i)) {
+      browserName = 'Opera';
+    } else if (userAgent.match(/edg/i)) {
+      browserName = 'Edge';
+    } else if (userAgent.match(/msie|trident/i)) {
+      browserName = 'Internet Explorer';
+    } else {
+      browserName = 'Unknown';
+    }
+
+    // Detect operating system
+    if (userAgent.match(/android/i)) {
+      osName = 'Android';
+    } else if (userAgent.match(/iphone|ipad|ipod/i)) {
+      osName = 'iOS';
+    } else if (userAgent.match(/win/i)) {
+      osName = 'Windows';
+    } else if (userAgent.match(/mac/i)) {
+      osName = 'MacOS';
+    } else if (userAgent.match(/linux/i)) {
+      osName = 'Linux';
+    } else if (userAgent.match(/unix/i)) {
+      osName = 'UNIX';
+    } else {
+      osName = 'Unknown';
+    }
+
+    return { browser: browserName, os: osName };
+  }
+
+  // Function to get screen resolution
+  function getScreenResolution() {
+    return {
+      width: window.screen.width,
+      height: window.screen.height
+    };
+  }
+
+
   // Helper function to generate a UUID
   function generateUUID() {
     var d = new Date().getTime();
@@ -46,17 +104,33 @@
     var domain = window.location.hostname;
     var pageUrl = window.location.href;
     var referrerUrl = document.referrer || '';
+    var referrerDomain = referrerUrl ? getDomainFromUrl(referrerUrl) : '';
+
+    // Add browser, OS, and screen resolution to the data object
+    var browserAndOS = getBrowserAndOS();
+    var screenResolution = getScreenResolution();
 
     var data = {
-      "type": "track",
-      "event": "pageView",
-      "properties": {
-        "domain": domain,
-        "pageUrl": pageUrl,
-        "referer": referrerUrl,
-        "anonymousId": uuid
+      type: "track",
+      event: "pageView",
+      context: {
+        page: {
+          path: window.location.pathname,
+          url: window.location.href,
+          title: document.title,
+          search: window.location.search,
+          referrer: document.referrer
+        },
+        userAgent: navigator.userAgent
+      },
+      properties: {
+        referrerDomain: referrerDomain,
+        anonymousId: uuid,
+        os: browserAndOS.os,
+        screenResolution: screenResolution
       }
     };
+
 
     var xhr = new XMLHttpRequest();
     xhr.open("POST", "https://e1.zinclabs.dev/v1/track", true);
