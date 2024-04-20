a63356f158f88d276ef163d55ec92b8bad2b3407
Prabhat Sharma <hi.prabhat@gmail.com>
2024-04-20 09:43:56 -0700

    add zoho

---
+++ public/js/zoho.js
@@ -0,0 +1,3 @@
+(function() {
+  var $zoho= $zoho || {};$zoho.salesiq = $zoho.salesiq || {widgetcode:"siqff3f17d787e2ca03164277c7a93ed5c624d858c9a5b988ba5a6f4875c2c0a751", values:{},ready:function(){}};var d=document;s=d.createElement("script");s.type="text/javascript"; s.id="zsiqscript";s.defer=true;s.src="https://salesiq.zoho.com/widget";t=d.getElementsByTagName("script")[0];t.parentNode.insertBefore(s,t);d.write("<div id='zsiqwidget'></div>");
+})();
