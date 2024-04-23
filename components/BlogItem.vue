6bcabc39c9d3ed55c18b87357b339513cf8f64e9
Kirtan Thakkar <kirtan@kiara.tech>
2024-04-23 19:52:16 +0530

    fix blog image path

--- components/BlogItem.vue
+++ components/BlogItem.vue
@@ -3,7 +3,7 @@
     <div
       class="shadow-md group hover:shadow-xl transition-all duration-300 rounded-xl border bg-white flex flex-col p-3"
     >
-      <img :src="'/' + item.img" class="object-cover rounded-xl" :alt="item.title"/>
+      <img :src="item.img" class="object-cover rounded-xl" :alt="item.title"/>
       <p class="mt-4 text-2xl font-bold">{{ item.title }}</p>
       <div
         class="flex items-center justify-end cursor-pointer mt-4 transition-all duration-300 group-hover:text-theme-primary-400"
